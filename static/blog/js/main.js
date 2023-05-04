ceb12cf326aaacc5187244c770e80d473add249a
anaysah <ajaysahu0606@gmail.com>
2023-05-04 21:06:15 +0530

    coresel on after header

--- static/blog/js/main.js
+++ static/blog/js/main.js
@@ -5,3 +5,23 @@ function changeTheme(event){
     icon.classList.toggle("fa-circle-half-stroke");
     icon.classList.toggle("fa-sun");
 }
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    injectStyles: [`
+    :root {
+        --swiper-pagination-bottom: auto;
+        --swiper-pagination-top: 8px;
+    }
+      `],
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
